3eb3912afe195aa7c8c4f66e2501e36fb026bed0
Jeff Surf <sofiane.sebbaghi2022@campus-eni.fr>
2023-10-09 16:59:15 +0200

    feat: migration participant add pseudo Correction des champs en fonction du MRD

---
+++ migrations/Version20231009142310.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231009142310 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE participant ADD pseudo VARCHAR(50) NOT NULL, CHANGE email email VARCHAR(100) NOT NULL, CHANGE nom nom VARCHAR(50) NOT NULL, CHANGE prenom prenom VARCHAR(50) NOT NULL, CHANGE telephone telephone VARCHAR(20) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D79F6B1186CC499D ON participant (pseudo)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_D79F6B1186CC499D ON participant');
+        $this->addSql('ALTER TABLE participant DROP pseudo, CHANGE email email VARCHAR(180) NOT NULL, CHANGE nom nom VARCHAR(255) NOT NULL, CHANGE prenom prenom VARCHAR(255) NOT NULL, CHANGE telephone telephone VARCHAR(10) NOT NULL');
+    }
+}
