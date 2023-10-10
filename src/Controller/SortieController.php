67539a346a8526603ffbfcecb0ca17f65b811b78
Guillaume VINCENT <guillaume.vincent12022@campus-eni.fr>
2023-10-10 09:45:38 +0200

    Sortie controller

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\SortieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/sortie', name: 'sortie')]
+class SortieController extends AbstractController
+{
+    #[Route('/list', name: '_list')]
+    public function list(SortieRepository $sortieRepository): Response
+    {
+        $sorties = $sortieRepository->findAll();
+
+        return $this->render('sortie/list.html.twig', [
+            'form' => $sorties
+        ]);
+    }
+}
