a12a53e21064899f4a0197e1a49722979c7a87c4
Jeff Surf <sofiane.sebbaghi2022@campus-eni.fr>
2023-10-13 14:17:39 +0200

    fix: redirect if password is the default set by admin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(UserPasswordHasherInterface $userPasswordHasher, Request $request): Response
     {
+        /** @var Participant $user */
+        $user = $this->getUser();
+        $passwordDefault = $user->getNom() . $user->getPrenom() . "@ENI2023";
+
+        if($userPasswordHasher->isPasswordValid($user, $passwordDefault))
+        {
+            return $this->redirectToRoute("app_profil_edit_password", ["pseudo" => $user->getPseudo()]);
+        }
+
         return $this->render('home/index.html.twig');
     }
 }
